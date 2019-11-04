1c18c5933ace2d553b46134e8331b6c846d43c42
MelLutten <57198988+MelLutten@users.noreply.github.com>
2019-11-04 10:45:31 +0100

    Update position.h

--- position.h
+++ position.h
@@ -1,36 +1,25 @@
 #ifndef POSITION_H
 #define POSITION_H
-#include<iostream>
 
+#include <iostream>
 
-
-
-
-namespace participant{
 
 class position{
 public:
+    position();
     position(int col,int lig);
     int numColonne()const;
     int numLigne()const;
     void changerPosition(int col,int lig);
+    void sauverPosition(std::ostream&ost);
+    void afficherPosition();
+
 private:
     int d_numColonne;
     int d_numLigne;
 
 };
 
-}
-
-
-
-
-
-
-
-
-
-
-
+std::ostream& operator<<(std::ostream&ost, position&p);
 
 #endif // POSITION_H
