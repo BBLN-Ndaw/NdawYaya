0db185ce9f36de37fa9c8c4d1d66503df5a366d2
MelLutten <57198988+MelLutten@users.noreply.github.com>
2019-11-04 10:44:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
 #include "grille.h"
+#include "joueur.h"
 #include <QApplication>
 #include "fenetre.h"
 
 int main(int argc, char *argv[])
 {
-    //joueur j;
-    terrain t(1,2,2,4,4);
+    position p;
+    std::string nom = "Jean";
+    joueur j{nom,p,0,0};
 
-    QApplication app{argc,argv};
+   // terrain t(4,3,1,5,5);
+    terrain t;
+   // t.afficheGrille();
+    t.ChangerJoueur(j);
+
+   QApplication app{argc,argv};
     fenetre f(t);
     f.show();
     app.exec();
+
+
 }
