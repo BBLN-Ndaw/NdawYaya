9866727a9599d9a5a9a7870675938d0775223929
MelLutten <57198988+MelLutten@users.noreply.github.com>
2019-11-04 10:46:10 +0100

    Update position.cpp

--- position.cpp
+++ position.cpp
@@ -1,22 +1,39 @@
-#include<iostream>
-#include"position.h"
-namespace participant{
+#include "position.h"
+
+position::position():d_numColonne{0},d_numLigne{0}
+{}
 
 position::position(int col,int lig):d_numColonne{col},d_numLigne{lig}
-{
-}
+{}
+
 int position::numColonne()const{
-return d_numColonne;
+    return d_numColonne;
 }
-position::numLigne()const{
-return d_numLigne;
+
+int position::numLigne()const{
+    return d_numLigne;
 }
+
 void position::changerPosition(int col,int lig){
-d_numColonne=col;
-d_numLigne=lig;
+
+    d_numColonne=col;
+    d_numLigne=lig;
+
 
 }
 
+void position::sauverPosition(std::ostream&ost){
+    ost<<"["<<d_numLigne<<","<<d_numColonne<<"]";
+}
+
+void position::afficherPosition(){
+    std::cout<<d_numLigne<<";"<<d_numColonne<<std::endl;
+}
+
+std::ostream& operator<<(std::ostream&ost, position&p){
+    p.sauverPosition(ost);
+    return ost;
+}
 
 
 
